0a12470b0f95706440a69c3ae50104ea0b47d05d
camilomartinezfo <caemartinezfo@unal.edu.co>
2021-06-06 23:53:30 -0500

    Modificado 2

--- SimNormalMultivariado.R
+++ SimNormalMultivariado.R
@@ -1,6 +1,3 @@
-nonlinearmod
-
-
 ## Simulación Monte Carlo para modelo con mínimos cuadrados
 
 predictNLS <- function(object, var.pred, newdata, level = 0.95, nsim = 10000, ...)
@@ -22,11 +19,11 @@ predictNLS <- function(object, var.pred, newdata, level = 0.95, nsim = 10000, ..
       NCOL <- ncol(VCOV)
       ADD1 <- c(rep(0, NCOL))
       ADD1 <- matrix(ADD1, ncol = 1)
-      colnames(ADD1) <- names(var.pred)
+      colnames(ADD1) <- var.pred
       VCOV <- cbind(VCOV, ADD1)
       ADD2 <- c(rep(0, NCOL + 1))
       ADD2 <- matrix(ADD2, nrow = 1)
-      rownames(ADD2) <- names(var.pred)
+      rownames(ADD2) <- var.pred
       VCOV <- rbind(VCOV, ADD2) 
       
       NR <- nrow(newdata)
@@ -38,8 +35,8 @@ predictNLS <- function(object, var.pred, newdata, level = 0.95, nsim = 10000, ..
             ## get predictor values and optional errors
             predVAL <- newdata[i, 1]
             predERROR <- 0
-            names(predVAL) <- names(var.pred)  
-            names(predERROR) <- names(var.pred) 
+            names(predVAL) <- var.pred
+            names(predERROR) <- var.pred
             
             ## create mean vector for 'mvrnorm'
             MU <- c(COEF, predVAL)
@@ -57,7 +54,7 @@ predictNLS <- function(object, var.pred, newdata, level = 0.95, nsim = 10000, ..
             
             ## collect statistics
             PRED <- data.frame(predVAL)
-            colnames(PRED) <- names(var.pred)   
+            colnames(PRED) <- var.pred
             FITTED <- predict(object, newdata = data.frame(PRED))
             MEAN.sim <- mean(EVAL, na.rm = TRUE)
             SD.sim <- sd(EVAL, na.rm = TRUE)
@@ -66,8 +63,19 @@ predictNLS <- function(object, var.pred, newdata, level = 0.95, nsim = 10000, ..
             outMAT <- rbind(outMAT, RES)
       }
       
-      colnames(outMAT) <- c("fit", "mean", "sd", "median", "mad", names(QUANT[1]), names(QUANT[2]))
+      colnames(outMAT) <- c("fit", "mean", "sd", names(QUANT[1]), names(QUANT[2]))
       rownames(outMAT) <- NULL
       
       return(outMAT)  
 }
+
+Ajustados <- predictNLS(object=nonlinearmod, var.pred = "Conc", newdata = data.frame(x.new))
+Ajustados <- as.data.frame(Ajustados)
+plot(data$Conc, data$Velocity, pch=20, col = "red", las =1,
+     xlab = "Concentration (ppm)",
+     ylab = expression(Velocity ~ (counts/min^2)),ylim=c(45,220))
+lines(seq(0.01,1.2,0.01),Ajustados$fit,lwd=2)
+lines(seq(0.01,1.2,0.01),Ajustados$`2.5%`,lwd=2, lty=3)
+lines(seq(0.01,1.2,0.01),Ajustados$`97.5%`,lwd=2, lty=3)
+
+
