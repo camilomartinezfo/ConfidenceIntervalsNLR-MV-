037c27a99e8703518a9f7a39da373547f7c75f8b
camilomartinezfo <caemartinezfo@unal.edu.co>
2021-06-09 22:55:27 -0500

    change

--- AbarcoExample.R
+++ AbarcoExample.R
@@ -100,6 +100,98 @@ ci.lines<-function(){
 plot(data$year,data$D,xlim=c(0,200),ylim=c(0,150),pch=16,bg="white",
      col="darkgray",cex=1,lwd=1.9,xlab="Age (years)",ylab="Diameter (cm)",cex.lab=1.2)
 ci.lines()
+curve(beta2.est[1]*((1-exp(-beta2.est[2]*x))^(beta2.est[3])), add = TRUE, col = "black", lwd =2)
 
+
+
+## IC: Monte Carlo method
+
+FittedML <- function(x){
+      diametro = beta2.est[1]*((1-exp(-beta2.est[2]*x))^(beta2.est[3]))
+      return(diametro)
+}
+
+predictNLS_MV <- function(EXPR, modelo, var.pred, newdata, level = 0.95, 
+                          nsim = 10000, ...)
+{
+      require(MASS, quietly = TRUE)
+      
+      ## coefficients
+      COEF <- modelo1.ML$par[-4]
+      
+      ## get variance-covariance matrix
+      FIM <- solve(modelo$hessian)
+      VCOV <- FIM[-4,-4]
+      rownames(VCOV) <- c("A","k","c")
+      colnames(VCOV) <- c("A","k","c")
+      
+      ## augment variance-covariance matrix for 'mvrnorm' 
+      ## by adding a column/row for 'error in x'
+      NCOL <- ncol(VCOV)
+      ADD1 <- c(rep(0, NCOL))
+      ADD1 <- matrix(ADD1, ncol = 1)
+      colnames(ADD1) <- var.pred
+      VCOV <- cbind(VCOV, ADD1)
+      ADD2 <- c(rep(0, NCOL + 1))
+      ADD2 <- matrix(ADD2, nrow = 1)
+      rownames(ADD2) <- var.pred
+      VCOV <- rbind(VCOV, ADD2)
+      
+      NR <- nrow(newdata)
+      varPLACE <- ncol(VCOV)
+      outMAT <- NULL  
+      
+      for (i in 1:NR) {
+            
+            ## get predictor values and optional errors
+            predVAL <- newdata[i, 1]
+            predERROR <- 0
+            names(predVAL) <- var.pred
+            names(predERROR) <- var.pred
+            
+            ## create mean vector for 'mvrnorm'
+            MU <- c(COEF, predVAL)
+            
+            ## create variance-covariance matrix for 'mvrnorm'
+            ## by putting error^2 in lower-right position of VCOV
+            newVCOV <- VCOV
+            newVCOV[varPLACE, varPLACE] <- predERROR^2
+            
+            ## create MC simulation matrix
+            simMAT <- mvrnorm(n = nsim, mu = MU, Sigma = newVCOV, 
+                              empirical = TRUE)
+            
+            ## evaluate expression on rows of simMAT
+            EVAL <- eval(EXPR, envir = as.data.frame(simMAT))
+            
+            ## collect statistics
+            PRED <- data.frame(predVAL)
+            colnames(PRED) <- var.pred
+            FITTED <- FittedML(data.frame(PRED))
+            MEAN.sim <- mean(EVAL, na.rm = TRUE)
+            SD.sim <- sd(EVAL, na.rm = TRUE)
+            QUANT <- quantile(EVAL, c((1 - level)/2, level + (1 - level)/2),
+                              na.rm=TRUE)
+            RES <- c(FITTED, MEAN.sim, SD.sim, QUANT[1], QUANT[2])
+            outMAT <- rbind(outMAT, RES)
+      }
+      
+      colnames(outMAT) <- c("fit", "mean", "sd", names(QUANT[1]), 
+                            names(QUANT[2]))
+      rownames(outMAT) <- NULL
+      
+      return(outMAT)  
+}
+
+
+Ajustados <- predictNLS_MV(EXPR=expression(A*((1-exp(-k*year))^c)), modelo=modelo1.ML, 
+                           var.pred = "year", newdata = data.frame(x.new))
+Ajustados <- as.data.frame(Ajustados)
+plot(data$year,data$D,xlim=c(0,200),ylim=c(0,150),pch=16,bg="white",
+     col="darkgray",cex=1,lwd=1.9,xlab="Age (years)",ylab="Diameter (cm)",cex.lab=1.2)
+curve(beta2.est[1]*((1-exp(-beta2.est[2]*x))^(beta2.est[3])), add = TRUE, col = "black", lwd =2)
+valort <- qt(0.975,45)
+lines(x.new,Ajustados$fit+Ajustados$sd*valort,lwd=2,lty=3)
+lines(x.new,Ajustados$fit-Ajustados$sd*valort,lwd=2,lty=3)
 
 
